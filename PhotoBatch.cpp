61a4c1947f7a5c6bb09b41586df700698e351e9c
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2022-11-25 13:20:05 -0400

    13. Implementar a classe ArgumentParser

--- PhotoBatch.cpp
+++ PhotoBatch.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
+#include<iomanip>
 
-int main()
+class ArgumentParser
+{
+public:
+	void RegisterFlag(const std::string& flag)
+	{
+
+	}
+
+	bool GetFlag(const std::string& flag)
+	{
+		return false;
+	}
+
+	void Parse(int argc, char* argv[])
+	{
+
+	}
+
+private:
+
+};
+
+int main(int argc, char* argv[])
 {	
 	setlocale(LC_ALL  , "portuguese");
-	std::cout << "Olá Mundo" << std::endl;
+	
+	ArgumentParser argParser;
+	argParser.RegisterFlag("rename");
+	argParser.RegisterFlag("convert");
+	argParser.RegisterFlag("resize");
+	argParser.RegisterFlag("scale");
+
+	argParser.Parse(argc, argv);
+
+	std::cout << std::boolalpha << "Rename: " << argParser.GetFlag("rename") << std::endl;
+	std::cout << std::boolalpha << "Convert: " << argParser.GetFlag("convert") << std::endl;
+	std::cout << std::boolalpha << "Resize: " << argParser.GetFlag("resize") << std::endl;
+	std::cout << std::boolalpha << "Scale: " << argParser.GetFlag("scale") << std::endl;
+
+
 	return 0;
 }
