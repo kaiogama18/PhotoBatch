e4d12ef5088913a586b732969e32ec39c91cba08
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2022-12-01 11:00:02 -0400

    39. Implementar o modo Rename - Parte 2

--- Mode.cpp
+++ Mode.cpp
@@ -5,6 +5,8 @@
 #include <array>
 #include <filesystem>
 
+#include "RenameMode.h"
+
 Mode::Mode(const std::string& filter, const std::string& folder)
 	: m_Filter{ filter }
 	, m_Folder{ folder }
@@ -26,25 +28,6 @@ void Mode::Run()
 {
 	RunImpl();
 }
-
-RenameMode::RenameMode(const std::string& filter, const std::string& folder, const std::string& prefix, int startNumber)
-	: Mode{ filter, folder }
-	, m_Prefix{ prefix }
-	, m_StartNumber{ startNumber }
-{}
-
-const std::string& RenameMode::GetModeName() const
-{
-	static const std::string RenameModeName = "[Rename]: ";
-	return RenameModeName;
-}
-
-void RenameMode::RunImpl()
-{
-	std::cout << GetModeName() << " IS Working";
-}
-
-// ------------------------------------
 
 const std::string& GetInvalidChars()
 {
--- Mode.h
+++ Mode.h
@@ -23,22 +23,6 @@ private:
 	std::string m_Folder;
 };
 
-class RenameMode final : public Mode
-{
-public:
-	RenameMode(const std::string& filter, const std::string& folder, const std::string& prefix, int startNumber);
-
-	const std::string& GetModeName() const override;
-
-protected:
-	void RunImpl() override;
-
-private:
-	std::string m_Prefix;
-	int m_StartNumber;
-
-};
-
 namespace Args
 {
 	namespace Flags
